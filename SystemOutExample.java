954a584edc9af7157afdf5f77ca941b5c6da2359
Ashutosh gautam <aashutoshgautam2611@gmail.com>
2022-08-20 20:20:38 +0530

    Create SystemOutExample.java

---
+++ SystemOutExample.java
@@ -0,0 +1,31 @@
+// How the JAVA System.out.println() and System.out.println() work?
+// What's the pattern java followed to print the string
+
+/*
+Here, I'm not going to explain how the java PrintStream works. I'm just going to represents the pattern of System.out.print... like how java assembles it..
+ */
+
+class CustomOutput {
+    // I'm just representing the code behaviour
+    public void println(String data) {
+        System.out.print(data + "\n");
+    }
+    public void print(String data) {
+        System.out.print(data);
+    }
+}
+
+class CustomSystem {
+    public static CustomOutput out = new CustomOutput();
+}
+
+class CustomMain {
+    public static void main(String gg[]) {
+        // let just test it
+        CustomSystem.out.println("hello");
+        CustomSystem.out.println("world");
+        CustomSystem.out.println("-------------------");
+        CustomSystem.out.print("hello");
+        CustomSystem.out.print("world");
+    }
+}
