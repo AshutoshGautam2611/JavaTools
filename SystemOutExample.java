db8279a30d1d44855bdb9a0c744f5fecbe43bd4a
Ashutosh gautam <aashutoshgautam2611@gmail.com>
2022-08-20 20:40:48 +0530

    Update SystemOutExample.java

--- SystemOutExample.java
+++ SystemOutExample.java
@@ -7,15 +7,50 @@ Here, I'm not going to explain how the java PrintStream works. I'm just going to
 
 class CustomOutput {
     // I'm just representing the code behaviour
+    // Something line PrintStream class
     public void println(String data) {
         System.out.print(data + "\n");
     }
+
     public void print(String data) {
         System.out.print(data);
+    }
+
+    public void println(long e) {
+        // some code to print the value of e
+    }
+
+    public void println(int e) {
+        // some code to print the value of e
+    }
+
+    public void println(double e) {
+        // some code to print the value of e
+    }
+
+    public void println(float e) {
+        // some code to print the value of e
+    }
+
+    public void println(char e) {
+        // some code to print the value of e
+    }
+
+    public void println(boolean e) {
+        // some code to print the value of e
+    }
+
+    public void println(Object e) {
+        // This is also the representation of the println when it receives a Object as a parameter it that case
+        // it just calls the toString method of that particular object.
+        // For example
+        // through (e) toString() method gets called and
+        // whatever toString returns, it gets printed
     }
 }
 
 class CustomSystem {
+    
     final public static CustomOutput out = new CustomOutput();
 }
 
